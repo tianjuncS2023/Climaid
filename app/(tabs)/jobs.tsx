5aab42c6aec3df69e2df2d821ae2e013a762c814
Yutong Xia <143774012+Yutong524@users.noreply.github.com>
2024-11-27 12:29:17 -0800

    add Job Main Page
    add job main page for both volunteers and event organizers

--- app/(tabs)/jobs.tsx
+++ app/(tabs)/jobs.tsx
@@ -1,10 +1,19 @@
-import { StyleSheet, Image } from "react-native";
+import { StyleSheet, Image, Pressable } from "react-native";
 
 import { ThemedText } from "@/components/ThemedText";
 import { ThemedView } from "@/components/ThemedView";
 import ParallaxScrollView from "@/components/ParallaxScrollView";
+import { router } from "expo-router";
+import { useRole, UserRole } from "@/contexts/RoleContext";
 
 export default function Jobs() {
+  const { role } = useRole();
+  const handleTakeQuiz = () => {
+    router.replace("/(tabs)\takeQuiz\takeQuiz");
+  }
+  const handleEditQuiz = () => {
+    router.replace("/(tabs)editQuizeditQuiz");
+  }
   return (
     <ParallaxScrollView
       headerBackgroundColor={{ light: "#A1CEDC", dark: "#1D3D47" }}
@@ -15,13 +24,42 @@ export default function Jobs() {
       <ThemedView style={styles.titleContainer}>
         <ThemedText type="title">Jobs</ThemedText>
       </ThemedView>
+      <Pressable style={styles.button} onPress={handleTakeQuiz}>
+          <ThemedText style={styles.buttonText}>Take Quiz</ThemedText>
+        </Pressable>
+      {role === UserRole.EVENT_ORGANIZER ? 
+        <Pressable style={styles.button} onPress={handleEditQuiz}>
+          <ThemedText style={styles.buttonText}>Edit Quiz</ThemedText>
+        </Pressable> : null}
     </ParallaxScrollView>
   );
 }
 
+
 const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    gap: 40,
+  },
+  button: {
+    backgroundColor: "#0a7ea4",
+    paddingVertical: 12,
+    paddingHorizontal: 48,
+    borderRadius: 8,
+    minWidth: 240,
+    alignItems: "center",
+  },
+  buttonText: {
+    color: "#fff",
+    fontSize: 18,
+    fontWeight: "600",
+  },
   titleContainer: {
-    flexDirection: "row",
-    gap: 8,
+    flexDirection: "column",
+    alignItems: "center",
+    marginTop: 220,
+    gap: 20,
   },
 });
