147e9d141a2e1627508c4d2413e20cc0bcf4415e
Herbie Rand <hjrand1005@gmail.com>
2024-11-27 14:13:56 -0800

    Roulette wheel scaffolding
    Signing this with CMU username/email

---
+++ app/(tabs)/roulette.tsx
@@ -0,0 +1,29 @@
+import React from "react";
+import { StyleSheet, Image } from "react-native";
+import RouletteWheel from "@/components/RouletteWheel";
+import { ThemedView } from "@/components/ThemedView";
+import ParallaxScrollView from "@/components/ParallaxScrollView";
+
+export default function Roulette() {
+	return (
+		<ParallaxScrollView
+			headerBackgroundColor={{ light: "#FFD6E0", dark: "#1F1D47" }}
+			headerImage={
+				<Image
+					source={require("@/assets/images/partial-react-logo.png")}
+				/>
+			}
+		>
+			<ThemedView style={styles.titleContainer}>
+				<RouletteWheel />
+			</ThemedView>
+		</ParallaxScrollView>
+	);
+}
+
+const styles = StyleSheet.create({
+	titleContainer: {
+		flexDirection: "row",
+		gap: 8,
+	},
+});
