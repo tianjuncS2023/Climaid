e133af2e65366608ca87f42803e947bf55e40cca
Herbie Rand <hrand@andrew.cmu.edu>
2024-11-30 16:38:40 -0800

    Fix spin-the-wheel tabbar navigation

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,5 +1,4 @@
 import { Tabs } from "expo-router";
-import React from "react";
 import { Platform } from "react-native";
 
 import { HapticTab } from "@/components/HapticTab";
@@ -77,6 +76,12 @@ export default function TabLayout() {
 							color={color}
 						/>
 					),
+				}}
+			/>
+			<Tabs.Screen
+				name="roulette"
+				options={{
+					href: null,
 				}}
 			/>
 		</Tabs>
